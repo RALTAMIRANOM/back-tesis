6400cd3924b3b4bcbd2a6bf58f2bc0c9dcc521f1
RENZO <martinez.renzo@pucp.edu.pe>
2020-11-07 23:19:47 -0500

    estructura de la api login v3

--- app.py
+++ app.py
@@ -80,7 +80,8 @@ def validated_User():
         'statusCode':200,
         'headers':{
             'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         }}
     else:
         return {'body' : json.dumps({'result':{"idPerson":-1}}), 
